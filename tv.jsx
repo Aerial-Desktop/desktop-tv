5bc3bb710f9807a43a6d2a17d2cf08ea99708d3b
michaeldimmitt <michaelgdimmitt@gmail.com>
2019-07-06 18:43:49 -0400

    beginning jsx implementation

---
+++ tv.jsx
@@ -0,0 +1,12 @@
+export const refreshFrequency = 5000 // ms
+
+export const render = () => (
+  <div>
+    <iframe src="https://giphy.com/embed/3jvujBL5wCSYM" width="480" height="358" frameBorder="0" class="giphy-embed" allowFullScreen />
+    
+    <iframe src="https://giphy.com/embed/Yohz12bnBkqVW" width="480" height="255" frameBorder="0" class="giphy-embed" allowFullScreen />
+    <iframe src="https://giphy.com/embed/3jvujBL5wCSYM" width="480" height="358" frameBorder="0" class="giphy-embed" allowFullScreen />
+    
+    <iframe src="https://giphy.com/embed/Yohz12bnBkqVW" width="480" height="255" frameBorder="0" class="giphy-embed" allowFullScreen />
+  </div>  
+)
