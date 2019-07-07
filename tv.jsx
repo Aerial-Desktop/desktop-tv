70b0e813506a4c5f641b84ee20080e1c2c02d777
michaeldimmitt <michaelgdimmitt@gmail.com>
2019-07-06 21:40:31 -0400

    put all the things in variables.

--- tv.jsx
+++ tv.jsx
@@ -16,27 +16,23 @@ export const className = `
 `
 
 export const render = () => (
-  <iframe src="https://threatmap.fortiguard.com" width="100%" height="100%" frameBorder="0"/>
+  <iframe src={threatMaps[0]} width="100%" height="100%" frameBorder="0"/>
 )
 
-/* 
-  Threat Maps: 
-// https://threatmap.fortiguard.com
-// https://threatmap.bitdefender.com
-// https://cybermap.kaspersky.com/widget
-// https://www.deteque.com/live-threat-map
-// https://www.fireeye.com/cyber-map/threat-map.html
-// https://threatbutt.com/map
+const threatMaps = [
+  'https://threatmap.fortiguard.com',
+  'https://threatmap.bitdefender.com',
+  'https://cybermap.kaspersky.com/widget',
+  'https://www.deteque.com/live-threat-map',
+  'https://www.fireeye.com/cyber-map/threat-map.html',
+  'https://threatbutt.com/map'
+]
 
-*/
-
-// const Gifs = () => (
-//   <div>
-//     <iframe src="https://giphy.com/embed/3jvujBL5wCSYM" width="480" height="358" frameBorder="0" class="giphy-embed" allowFullScreen />
-    
-//     <iframe src="https://giphy.com/embed/Yohz12bnBkqVW" width="480" height="255" frameBorder="0" class="giphy-embed" allowFullScreen />
-//     <iframe src="https://giphy.com/embed/3jvujBL5wCSYM" width="480" height="358" frameBorder="0" class="giphy-embed" allowFullScreen />
-    
-//     <iframe src="https://giphy.com/embed/Yohz12bnBkqVW" width="480" height="255" frameBorder="0" class="giphy-embed" allowFullScreen />
-//   </div>
-// );
+const Gifs = () => (
+  <div>
+    <iframe src="https://giphy.com/embed/3jvujBL5wCSYM" width="480" height="358" frameBorder="0" class="giphy-embed" allowFullScreen />   
+    <iframe src="https://giphy.com/embed/Yohz12bnBkqVW" width="480" height="255" frameBorder="0" class="giphy-embed" allowFullScreen />
+    <iframe src="https://giphy.com/embed/3jvujBL5wCSYM" width="480" height="358" frameBorder="0" class="giphy-embed" allowFullScreen />
+    <iframe src="https://giphy.com/embed/Yohz12bnBkqVW" width="480" height="255" frameBorder="0" class="giphy-embed" allowFullScreen />
+  </div>
+);
