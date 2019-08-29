97441546e5c65356af25cc3de0199299134e519d
michaeldimmitt <michaelgdimmitt@gmail.com>
2019-08-29 16:20:18 -0400

    only show one plugin by keeping in a single jsx file.

--- tv.jsx
+++ tv.jsx
@@ -1,12 +1,50 @@
-import { video_experiences } from './videos.jsx'
-import { randomArrValue, randomArrNumber, buildUrlPlaylist, buildLiveChannelsUrl, buildLiveVideo } from './helpers.jsx'
-const { live_news_if_online, static_news, live_music, code_pen, entertainment_and_visuals } = video_experiences
 
+const helpers = {
+  randomArrValue: (arr) => arr[ Math.floor((Math.random() * arr.length)) ],
+  randomArrNumber: (num) => Math.floor((Math.random() * num)),
+  buildUrlPlaylist: (plist) => 'https://www.youtube.com/embed/?list=' + plist + '&index=1&rel=0&autoplay=1',
+  buildLiveChannelsUrl: (channel) => 'https://www.youtube.com/embed/live_stream?channel=' + channel + '&autoplay=1',
+  buildLiveVideo: (plist) => 'https://www.youtube.com/embed/' + plist + '?index=1&rel=0&autoplay=1'
+}
+const { randomArrValue, randomArrNumber, buildUrlPlaylist, buildLiveChannelsUrl, buildLiveVideo } = helpers
+const video_experiences = {
+  live_news_if_online : {
+    spacexLiveChecker: buildLiveChannelsUrl('UCtI0Hodo5o5dUb67FeUjDeA'),
+    podcastLiveChecker: buildLiveChannelsUrl('UCzQUP1qoWDoEbmsQxvdjxgQ'),
+    youtubeLiveNowTechnology: buildUrlPlaylist('PL57quI9usf_th5iJjjhXcRzlzibHUgYMA'),
+    youtubeLiveNowAnimals: buildUrlPlaylist('PLIFqWCuxNyoj8HAwNYOlqdDL52pNsbvKV')
+  },
+  static_news: {   /* static news updated daily */
+    bbcTopStories: buildUrlPlaylist('PLS3XGZxi7cBVNadbxDqZCUgISvabEpu-g'),
+    espnMustSee: buildUrlPlaylist('PLn3nHXu50t5xa7-HYnJpzN5cxkLYgFP_V')
+  },
+  live_music: {
+    youtubeChilledCowByVideo: buildLiveVideo('hHW1oY26kxQ'),
+    youtubeChilledCowByChannelId: buildLiveChannelsUrl('UCSJ4gkVC6NrvII8umztf0Ow')
+  },
+  code_pen: {
+    hyperSpace: 'https://codepen.io/yuanchuan/full/dqrdow',
+    enterTheMatrix: 'https://codepen.io/yuanchuan/full/YoqWeR',
+    dnaSequence: 'https://codepen.io/yuanchuan/full/pBrMNQ',
+    trippy: 'https://codepen.io/yuanchuan/full/wZJqNK',
+    pitfall: 'https://codepen.io/dsenneff/full/RqYONv'
+  },
+  entertainment_and_visuals: {
+    creedLoadscreens: 'PLZbIo_dYB77tFXtSy6nZjnRyiH_Hwqe_B',
+    assassinsCreedTrailers: 'PLOcTEsKp5qpV7pbtiGeLAjlmD086bqymr',
+    lofiAnimes: 'PLZbIo_dYB77tkPQCgwRMq9a_RRkq0UXZ3',
+    historyOfArchitectureTestWillFailLaterOn: 'PL7_CkZ72ky5P1aPgHTbLBZbcddN_48F44',
+    birds:'PLOh2AUhKQzaNeE-vXiH1SMeJyTdRT84dr'
+  }
+}
+const { live_news_if_online, static_news, live_music, code_pen, entertainment_and_visuals } = video_experiences
 const { spacexLiveChecker, podcastLiveChecker, youtubeLiveNowTechnology, youtubeLiveNowAnimals } = live_news_if_online
 const { bbcTopStories, espnMustSee } = static_news
 const { youtubeChilledCowByVideo, youtubeChilledCowByChannelId } = live_music
 const { hyperSpace, enterTheMatrix, dnaSequence, trippy, pitfall } = code_pen
 const { creedLoadscreens, assassinsCreedTrailers, lofiAnimes, historyOfArchitectureTestWillFailLaterOn, birds } = entertainment_and_visuals
+
+
 
 const threatMaps = [
   'https://threatmap.fortiguard.com',
